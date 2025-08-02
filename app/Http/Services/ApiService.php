f0d58bb436a9152a82aa3d583982e52e44deab01
germanusk <germanuskeming@gmail.com>
2025-08-02 14:39:23 +0100

    ------

--- app/Http/Services/ApiService.php
+++ app/Http/Services/ApiService.php
@@ -86,12 +86,7 @@ class ApiService{
     public function max_matric($prefix, $year, $suffix=null)
     {
         # code...
-        try {
-            //code...
-            return Http::get(Helpers::instance()->getApiRoot().'/'.config('api_routes.max_matric').'/'.$prefix.'/'.$year.'/'.$suffix)->body();
-        } catch (\Throwable $th) {
-            throw $th;
-        }
+        return Http::get(Helpers::instance()->getApiRoot().'/'.config('api_routes.max_matric').'/'.$prefix.'/'.$year.'/'.$suffix)->body();
     }
 
     public function matric_exist($matric)
