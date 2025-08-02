819b315d2528311e014508b4daaad55a0541b9f1
germanusk <germanuskeming@gmail.com>
2025-08-02 14:11:56 +0100

    debugging

--- app/Http/Controllers/Admin/ProgramController.php
+++ app/Http/Controllers/Admin/ProgramController.php
@@ -1144,7 +1144,7 @@ class ProgramController extends Controller
                     if($prefix == null){
                         return back()->with('error', 'Matricule generation prefix not set.');
                     }
-                    $max_matric = $this->api_service->max_matric($prefix, $year)->collect('data'); //matrics starting with '$prefix' sort
+                    $max_matric = json_decode($this->api_service->max_matric($prefix, $year))->data; //matrics starting with '$prefix' sort
                     // dd($max_matric);
                     if($max_matric == null){
                         $max_count = 0;
