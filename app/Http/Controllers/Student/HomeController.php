e03ef288bc6d7a4a63ffa43145d174eea79604a7
germanusk <germanuskeming@gmail.com>
2025-08-09 23:06:42 +0100

    -----

--- app/Http/Controllers/Student/HomeController.php
+++ app/Http/Controllers/Student/HomeController.php
@@ -326,7 +326,7 @@ class HomeController extends Controller
             $data = collect($data)->filter(function($value, $key){return $key != '_token';})->toArray();
             $application = ApplicationForm::updateOrInsert(['id'=> $application_id, 'student_id'=>auth('student')->id()], $data);
         }elseif($step == 7){
-            dd($request->all());
+            // dd($request->all());
             $tk_counter = 0;
             $application = auth('student')->user()->applicationForms()->where('year_id', Helpers::instance()->getCurrentAccademicYear())->first();
             if($application->degree_id == null){ goto SKIP;}
@@ -365,6 +365,7 @@ class HomeController extends Controller
             }else{
                 $request_data = ['mobileWalletNumber'=>str_replace('+', '', strlen($request->momo_number) == 9 ? '237'.$request->momo_number : $request->momo_number), 'mchTransactionRef'=>'_apl_fee_'.time().'_'.random_int(1, 9999), "amount"=> $request->amount, "currencyCode"=> "XAF", "description"=>"Payment for application fee into HIMS"];
                 $_response = Http::withHeaders($headers)->post(config('tranzak.base').config('tranzak.direct_payment_request'), $request_data);
+                dd($_response->collect());
                 if($_response->status() == 200){
                     
                     // $_data = $_response->collect();
