b8ebeb90ba2469b1afff7f8274663b0ebd48e3fc
germanusk <germanuskeming@gmail.com>
2025-08-26 09:05:10 +0100

    admission report with excel download complete

--- app/Http/Controllers/Admin/ProgramController.php
+++ app/Http/Controllers/Admin/ProgramController.php
@@ -676,7 +676,7 @@ class ProgramController extends Controller
         // $data['programs'] = collect(json_decode($this->api_service->programs())->data);
         // $data['degrees'] = collect(json_decode($this->api_service->degrees())->data);
         // $data['certificates'] = collect(json_decode($this->api_service->certificates())->data);
-        $data['structure'] = $this->api_service->school_program_structure()->get('data');
+        $data['structure'] = collect($this->api_service->school_program_structure()->get('data'));
         $data['applications'] = ApplicationForm::whereNotNull('matric')->orderBy('name')->get()->each(function($rec) use($data){
             $rec->department = $data['structure']->where('program_id', $rec->program)->first()?->department??'';
         });
