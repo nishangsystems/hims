19782ecddccda8721f30515c9b7eee6add1e9edb
germanusk <germanuskeming@gmail.com>
2025-08-26 13:02:32 +0100

    -------

--- app/Http/Controllers/Student/HomeController.php
+++ app/Http/Controllers/Student/HomeController.php
@@ -425,7 +425,7 @@ class HomeController extends Controller
             // $data = $request->all();
             if($request->program != null){
                 $levels = collect(json_decode($this->api_service->campusProgramLevels($application->campus_id, $request->program))->data);
-                $data['level'] = $levels->first()['level']??'';
+                $data['level'] = $levels->first()?->level??'';
             }
             $data = collect($data)->filter(function($value, $key){return $key != '_token';})->toArray();
             $application = ApplicationForm::updateOrInsert(['id'=> $application_id, 'student_id'=>auth('student')->id()], $data);
